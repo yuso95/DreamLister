7291b49d5eec2eda3c8a336956b3aed480368712
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-12-01 13:56:18 -0500

    Added IBOutlets for the ItemDetailVC and generate some test data from Core Data

--- DreamLister/ItemDetailVC.swift
+++ DreamLister/ItemDetailVC.swift
@@ -7,29 +7,93 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailVC: UIViewController {
+class ItemDetailVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var detailField: UITextField!
+    
+    var stores = [Store]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        
+        storePicker .delegate = self
+        storePicker.dataSource = self
+        
+//        generateTestData()
+        
+        getStores()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+         let store = stores[row]
+        
+        return store.name
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return stores.count
     }
-    */
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        // Update it later
+        
+        
+    }
+    
+    // Core data functions
+    
+    func getStores() {
+        
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            
+            let error = error as NSError
+            
+            print(error)
+        }
+        
+    }
+    
+    func generateTestData() {
+        
+        let store1 = Store(context: context)
+        store1.name = "Best Buy"
+        
+        let store2 = Store(context: context)
+        store2.name = "Amazon"
+        
+        let store3 = Store(context: context)
+        store3.name = "K Mart"
+        
+        let store4 = Store(context: context)
+        store4.name = "Fry Electronics"
+        
+        let store5 = Store(context: context)
+        store5.name = "Target"
+        
+        ad.saveContext()
+    }
 }
